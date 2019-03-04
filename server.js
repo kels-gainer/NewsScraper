2a989c258499970c33ca932428bea1f2d79129cd
Kelsey <kelsgainer@gmail.com>
2019-03-04 09:28:40 -0700

    rough draft

--- server.js
+++ server.js
@@ -3,23 +3,25 @@ var logger = require("morgan");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
-
 var db = require("./models");
-var PORT = 8000;
+var PORT = process.env.PORT || 8000;;
 var app = express();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.use(express.json());
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
 mongoose.connect(MONGODB_URI);
 
 var exphbs = require('express-handlebars');
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+require('./routes/routes')(app);
+
+// move to routes
 
 app.get("/scrape", function(req, res) {
     axios.get("https://www.nytimes.com/").then(function(response) {
